885d6475ea0279f6005f36dbe54a2d27f6e83e5b
Shota Aso <117519628+suzukaze-pix@users.noreply.github.com>
2024-06-23 13:00:43 +0900

    Update main.py
    おジャ魔女どれみ

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 import random
+import cv2
+import torch
+
 
 muimi_list = ["ModuleNotFoundError: Universe Not Found","Fatal Python error: Unicorn Buffer Overflow","TypeError: Demonic Possession Detected","AttributeError: Time Traveler Conflict",'TypeError: Book Burn Detected',"TypeError: Comic Sans Detected","NameError: Future Not Found","AttributeError: No Funk Music detected","AttributionError: Insane Request"]
 
@@ -7,7 +10,29 @@ muimi_list = ["ModuleNotFoundError: Universe Not Found","Fatal Python error: Uni
 app = FastAPI()
 @app.get("/")
 def Send_muimi():
+    blank()
     return NoImiSellect()
 
 def NoImiSellect():
-        return random.choice(muimi_list)
+    return random.choice(muimi_list)
+
+def blank():
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#グランドマザー・テレサ
